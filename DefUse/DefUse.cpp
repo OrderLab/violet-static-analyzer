efda05b5abaebad5dc24ea6e72c4b1bd2199f718
Yigong Hu <hyigong1@jhu.edu>
2019-11-27 19:50:28 -0500

    Refine the defUse pass to trace the store and load instruction

--- DefUse/DefUse.cpp
+++ DefUse/DefUse.cpp
@@ -12,43 +12,37 @@
 
 using namespace llvm;
 
-template<typename T>
-void getVariableUse(T *variable);
-
-void getArgumentUse(Function *fn, int position);
-
-void printIndentation();
-
-template<typename T>
-void getTaintVariableUse(Value *taintedVariable, StoreInst *startPoint, bool isTainted);
-
-template<typename T>
-void checkInstructionUse(Instruction *instruction, T *variable, bool isTainted);
 
 namespace {
     static cl::opt<std::string> gVariableName("v", cl::desc("Specify variable name"), cl::value_desc("variablename"));
 //    static cl::list <int> OffsetList("offset", cl::desc("Specify the position of the memeber in a struct variable"));
 
-    int indentation = 0;
-    std::list<Instruction *> taintedVariableList;
+    class DefUse : public ModulePass {
+        template<typename T>
+        void getVariableUse(T *variable);
+        void printIndentation();
 
-    struct DefUse : public ModulePass {
+    public:
         static char ID; // Pass identification, replacement for typeid
         int mIndex = 0;
         int OffsetList[2];
         std::ofstream result_log;
+        int indentation = 0;
         bool flag = false;
 
         DefUse() : ModulePass(ID) {}
 
-
+        /*
+         * Perform the static analysis to each module,
+         */
         bool runOnModule(Module &M) override {
+            //TODO: automatically bound the variable name to the offset
             OffsetList[0] = 14;
             OffsetList[1] = 10;
             result_log.open("result.log");
             errs() << "We want to trace the uses of variable: " << gVariableName << "\n";
 
-            //check whether it is a global variable
+            //If the variable is a global variable, get the variable usage
             for (auto &Global : M.getGlobalList()) {
                 if (Global.getName() == gVariableName) {
                     errs() << "It is a global variable " << Global.getName() << "\n";
@@ -57,129 +51,141 @@ namespace {
                 }
             }
 
-            //check whether it is a common variable
-            for (Module::iterator moduleInterator = M.begin(), moduleEnd = M.end();
-                 moduleInterator != moduleEnd; moduleInterator++) {
-                for (Function::iterator functionInterator = moduleInterator->begin(), functionEnd = moduleInterator->end();
-                     functionInterator != functionEnd; ++functionInterator) {
-                    for (BasicBlock::iterator blockInterator = functionInterator->begin(), blockEnd = functionInterator->end();
-                         blockInterator != blockEnd; blockInterator++) {
-                        Instruction *inst = dyn_cast<Instruction>(blockInterator);
-                        if (inst->getName() == gVariableName) {
-//                            errs() << "It is a local variable defined in Function \"" << moduleInterator->getName()
-//                                   << "\"\n";
-//                            errs() << "The definition is" << *inst << "\n";
-                            getVariableUse(inst);
+            //If the variable is a local variable or an argument, get the variable usage
+            for (Module::iterator function = M.begin(), moduleEnd = M.end();
+                 function != moduleEnd; function++) {
+
+                if (function->getName() == "thd_test_options" && !flag) {
+                    flag = true;
+                    for (SymbolTableList<Argument>::iterator arg = function->arg_begin(); arg != function->arg_end(); arg++) {
+                        if (arg->getName() == gVariableName) {
+                            errs() << "It is a argument of Function \""
+                                   << arg->getParent()->getName()
+                                   << "\"\n";
+                            errs() << "The definition is " << *arg << "\n";
+                            getVariableUse(&(*arg));
                         }
+                    }
+                }
 
-                        if (CallInst *callInst = dyn_cast<CallInst>(inst)) {
-                            Function *callee = callInst->getCalledFunction();
-                            if (!callee)
-                                continue;
-                            if (callee->getName() == "thd_test_options") {
-                                errs() << "Yigong Hu\n";
-                                flag = true;
-                            }
-                            for (auto arg = callee->arg_begin(); arg != callee->arg_end(); arg++) {
-                                if (arg->getName() == gVariableName) {
-//                                    errs() << "It is a argument of Function \""
-//                                           << callInst->getCalledFunction()->getName()
-//                                           << "\"\n";
-//                                    errs() << "The definition is " << *arg << "\n";
-                                    if (flag) {
-                                        errs() << "the arg is " << *arg << "\n";
-                                    }
-                                    getVariableUse(arg);
-                                }
-                            }
-                            flag = false;
-
+                for (Function::iterator block = function->begin(), functionEnd = function->end();
+                     block != functionEnd; ++block) {
+                    for (BasicBlock::iterator instruction = block->begin(), blockEnd = block->end();
+                         instruction != blockEnd; instruction++) {
+                        Instruction *inst = dyn_cast<Instruction>(instruction);
+                        if (inst->getName() == gVariableName && !inst->user_empty()) {
+                            errs() << "It is a local variable defined in Function \"" << function->getName()
+                                   << "\"\n";
+                            errs() << "The definition is" << *inst << "\n";
+                            getVariableUse(inst);
                         }
                     }
                 }
             }
             return false;
         }
+    };
 
-        template<typename T>
-        void getVariableUse(T variable) {
-            std::vector<Value::use_iterator> worklist;
-            bool isStruct = false;
-            bool isClass = false;
+    /*
+    * Find all the instructions that use the target variable. If the variable is assigned to other variable, also find the
+    * usage of the other variable
+    *
+    * @param variable the target variable
+    */
+    template<typename T>
+    void DefUse::getVariableUse(T* variable) {
+        std::vector<Value::use_iterator> worklist;
+        bool isStruct = false;
+        bool isClass = false;
 
-            if (variable->getType()->isPointerTy() && variable->getType()->getPointerElementType()->isStructTy()) {
-//                errs() << "Variable is a struct variable\n";
-                isStruct = true;
-            }
+        if (variable->getType()->isPointerTy() && variable->getType()->getPointerElementType()->isStructTy()) {
+            errs() << "Variable is a struct variable " << *variable <<"\n";
+            isStruct = true;
+        }
 
-            if (variable->getType()->isPointerTy() && variable->getType()->getPointerElementType()->isPointerTy() &&
-                variable->getType()->getPointerElementType()->getPointerElementType()->isStructTy()) {
-//                errs() << "Variable is a class variable\n";
-                isClass = true;
-            }
+        if (variable->getType()->isPointerTy() && variable->getType()->getPointerElementType()->isPointerTy() &&
+            variable->getType()->getPointerElementType()->getPointerElementType()->isStructTy()) {
+            errs() << "Variable is a point variable that points to a struct" << *variable <<"\n";
+            isClass = true;
+        }
 //                errs() << "Variable is used in instruction:\n";
-            for (User *U : variable->users()) {
-                if (flag) {
-                    errs() << "the usage is " << *U << "\n";
-                }
-                if (Instruction *Inst = dyn_cast<Instruction>(U)) {
-                    if (isStruct) {
-                        if (isa<GetElementPtrInst>(Inst)) {
-                            GetElementPtrInst *getElementPtrInst = dyn_cast<GetElementPtrInst>(Inst);
-                            Type *operandType = getElementPtrInst->getPointerOperand()->getType();
-                            Type *pointValueType = operandType->getPointerElementType();
-                            ConstantInt *structOffset = dyn_cast<ConstantInt>(getElementPtrInst->getOperand(2));
-                            if (operandType->isPointerTy() && pointValueType->isStructTy()) {
-                                if (structOffset->getValue() == OffsetList[mIndex]) {
-                                    mIndex++;
-                                    printIndentation();
-                                    errs() << "The definition is " << *Inst << "\n";
-//                                    errs() << *Inst << "\n";
-                                    getVariableUse(Inst);
-                                    mIndex--;
-                                }
+        for (User *U : variable->users()) {
+            if (Instruction *Inst = dyn_cast<Instruction>(U)) {
+                errs() << *Inst << "\n";
+                if (isStruct) {
+                    if (StoreInst * storeInst = dyn_cast<StoreInst>(Inst)) {
+                        if (storeInst->getValueOperand() == variable) {
+                            getVariableUse(storeInst->getPointerOperand());
+                        }
+                    }
+                    if (isa<GetElementPtrInst>(Inst)) {
+                        GetElementPtrInst *getElementPtrInst = dyn_cast<GetElementPtrInst>(Inst);
+                        Type *operandType = getElementPtrInst->getPointerOperand()->getType();
+                        Type *pointValueType = operandType->getPointerElementType();
+                        ConstantInt *structOffset = dyn_cast<ConstantInt>(getElementPtrInst->getOperand(2));
+                        if (operandType->isPointerTy() && pointValueType->isStructTy()) {
+                            if (structOffset->getValue() == OffsetList[mIndex]) {
+                                errs() << "The offset is " << OffsetList[mIndex] << "\n";
+                                mIndex++;
+                                printIndentation();
+//                                    errs() git s<< *Inst << "\n";
+                                getVariableUse(Inst);
+                                errs() << "----------------\n";
+                                mIndex--;
                             }
                         }
-                    } else if (isClass) {
-                        if (flag) {
-                            errs() << "the ins is " << *Inst << "\n";
+                    }
+                } else if (isClass) {
+                    if (StoreInst *storeInst = dyn_cast<StoreInst>(Inst)) {
+                        if (storeInst->getValueOperand() == variable) {
+                            getVariableUse(storeInst->getPointerOperand());
                         }
-                        if (isa<GetElementPtrInst>(Inst)) {
-                            GetElementPtrInst *getElementPtrInst = dyn_cast<GetElementPtrInst>(Inst);
-                            Type *operandType = getElementPtrInst->getPointerOperand()->getType();
-                            Type *pointValueType = operandType->getPointerElementType();
-                            ConstantInt *structOffset = dyn_cast<ConstantInt>(getElementPtrInst->getOperand(2));
-                            errs() << "Yigong Hu\n";
-                            if (operandType->isPointerTy() && pointValueType->isPointerTy() && pointValueType->getPointerElementType()->isPointerTy()) {
-                                if (structOffset->getValue() == OffsetList[mIndex]) {
-                                    mIndex++;
-                                    printIndentation();
-                                    errs() << "The definition is " << *Inst << "\n";
-//                                    errs() << *Inst << "\n";
-                                    getVariableUse(Inst);
-                                    mIndex--;
-                                }
-                            }
-                        }
-
-                    } else {
-//                        errs() << "The definition is " << *Inst << "\n";
-                        errs() << *Inst << "\n";
                     }
 
+                    if (LoadInst *loadInst = dyn_cast<LoadInst>(Inst)) {
+                        if (loadInst->getPointerOperand() == variable) {
+                            getVariableUse(loadInst);
+                        }
+                    }
+
+                    if (isa<GetElementPtrInst>(Inst)) {
+                        GetElementPtrInst *getElementPtrInst = dyn_cast<GetElementPtrInst>(Inst);
+                        Type *operandType = getElementPtrInst->getPointerOperand()->getType();
+                        Type *pointValueType = operandType->getPointerElementType();
+                        ConstantInt *structOffset = dyn_cast<ConstantInt>(getElementPtrInst->getOperand(2));
+                        if (operandType->isPointerTy() && pointValueType->isPointerTy() && pointValueType->getPointerElementType()->isPointerTy()) {
+                            if (structOffset->getValue() == OffsetList[mIndex]) {
+                                mIndex++;
+                                printIndentation();
+                                errs() << *Inst << "\n";
+                                getVariableUse(Inst);
+                                mIndex--;
+                            }
+                        }
+                    }
+                } else {
+                    if (StoreInst *storeInst = dyn_cast<StoreInst>(Inst)) {
+                        if (storeInst->getValueOperand() == variable) {
+                            getVariableUse(storeInst->getPointerOperand());
+                        }
+                    }
+                    if (LoadInst *loadInst = dyn_cast<LoadInst>(Inst)) {
+                        if (loadInst->getPointerOperand() == variable) {
+                            getVariableUse(loadInst);
+                        }
+                    }
+                    errs() << *Inst << "\n";
                 }
             }
-
-
         }
+    }
 
-
-        void printIndentation() {
-            for (int i = 0; i < indentation; i++) {
-                errs() << "\t";
-            }
+    void DefUse::printIndentation() {
+        for (int i = 0; i < indentation; i++) {
+            errs() << "\t";
         }
-    };
+    }
+
 }
 
 char DefUse::ID = 0;
